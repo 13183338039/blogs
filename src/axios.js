fff111976619a229d53ab2966c596491467d032d
13183338039 <93919727+13183338039@users.noreply.github.com>
2025-02-04 23:17:47 +0800

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 
-axios.defaults.baseURL = "http://localhost:8081"
+axios.defaults.baseURL = "http://47.122.119.21:8081"
 
 // 前置拦截
 axios.interceptors.request.use(config => {
